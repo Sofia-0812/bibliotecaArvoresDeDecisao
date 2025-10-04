23bd1a320da87b7230c07c613f6044d7384ef4af
Sofia Grossi Vieira Santos <164202874+Sofia-0812@users.noreply.github.com>
2025-10-03 21:50:08 -0300

    Update __init__.py

--- mytrees/__init__.py
+++ mytrees/__init__.py
@@ -1,3 +1,21 @@
-from .tree import DecisionTree
+from .tree import (
+    BaseDecisionTreeClassifier,
+    ID3DecisionTreeClassifier,
+    C45DecisionTreeClassifier,
+    CARTDecisionTreeClassifier,
+    calculate_entropy,
+    calculate_information_gain,
+    calculate_gini,
+    split_info,
+)
 
-__all__ = ["DecisionTree"]
+__all__ = [
+    "BaseDecisionTreeClassifier",
+    "ID3DecisionTreeClassifier",
+    "C45DecisionTreeClassifier",
+    "CARTDecisionTreeClassifier",
+    "calculate_entropy",
+    "calculate_information_gain",
+    "calculate_gini",
+    "split_info",
+]
